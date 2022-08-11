5ce8f48d59d1f299a84f9ccf3ba8ab209188a820
seeyoniga <64161066+seeyoniga@users.noreply.github.com>
2022-08-11 15:39:04 +0530

    committed

---
+++ lesson3-assisstedproject3/src/main/java/com/demo/Main.java
@@ -0,0 +1,37 @@
+package com.demo;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		StandardServiceRegistry registry=  new StandardServiceRegistryBuilder()
+				.configure("hibernate.cfg.xml").build();
+		
+		Metadata metadata=  new MetadataSources(registry).getMetadataBuilder().build();
+		SessionFactory factory= metadata.getSessionFactoryBuilder().build();
+				
+				Session session= factory.openSession();
+				
+				Student s1=new Student("leah", "leahnega@gmail.com", "okay123");
+				session.save(s1);
+				
+				Transaction tx= session.beginTransaction();
+								
+				tx.commit();
+				
+			
+				System.out.println("Data Inserted Succesfully");
+	}
+
+}
